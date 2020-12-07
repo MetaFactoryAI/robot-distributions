6c8120c9aff574b27fda281e520aa31b770c0181
Hammad Jutt <jutt@ualberta.ca>
2020-12-07 01:32:33 -0700

    Publish Dec 2020 Distribution Script and Data

---
+++ README.md
@@ -0,0 +1,12 @@
+## $ROBOT Token Distribution Calculator
+
+This repo contains the scripts + sales data used to calculate $ROBOT token allocations
+for buyers and designers of products sold on MetaFactory. The sales data has been scrubbed
+of any personal information, but the products and sales records are public so that community members
+can verify the token allocations themselves.
+
+[Spreadsheet of Distribution Schedule/History](https://docs.google.com/spreadsheets/d/1gKSymD26W1UfYH81GXJwXHDsuupc31XQkH1VGnktZrI/edit?usp=sharing)
+
+
+The sales data for the Dec 2020 buyer/designer distribution can be found in the `dec2020` folder in the repo. This includes
+all shop purchases from Jan 1, 2020 to Dec 06, 2020. The code used to calculate the distribution can be found in `index.ts`.
---
+++ index.ts
@@ -0,0 +1,131 @@
+import { gql, GraphQLClient } from "graphql-request/dist";
+import _ from 'lodash';
+import ALL_ORDERS from './dec2020/sales_2020-01-01_2020-12-06-2.json';
+import Web3 from 'web3';
+require('dotenv').config()
+
+const BUYER_ROBOT_PER_DOLLAR = 0.4;
+const DESIGNER_ROBOT_PER_DOLLAR = 0.16;
+
+const ENDPOINT = process.env.GRAPHQL_ENDPOINT;
+const TOKEN = process.env.ACCESS_TOKEN;
+
+if (!ENDPOINT || !TOKEN) {
+  throw new Error('Missing ENV Variables.')
+}
+
+const client = new GraphQLClient(ENDPOINT, { headers: { 'X-Shopify-Access-Token': TOKEN } })
+
+
+const web3 = new Web3("http://geth.dappnode:8545")
+
+const numberToWei = (n: number) => web3.utils.toWei(n.toFixed(5), 'ether')
+
+const calculateBuyerAllocation = async () => {
+  const buyers = _(ALL_ORDERS)
+    .groupBy('customer_id')
+    .mapValues(
+      orders => orders.reduce((acc, order) => ({
+        totalSpent: acc.totalSpent + order.net_sales,
+        customerId: order.customer_id,
+      }), { totalSpent: 0, customerId: 0 })
+    ).values().value();
+
+  const recipients: Array<{ ethAddress: string, tokenAmount: number}> = [];
+  let totalSales = 0;
+
+  // map buyer purchases to ETH Addresses and token allocation
+  for (const b of buyers) {
+    totalSales += b.totalSpent;
+    const ethAddress = await getEthAddressForCustomer(b.customerId);
+    if (ethAddress) {
+      recipients.push({ ethAddress, tokenAmount: b.totalSpent * BUYER_ROBOT_PER_DOLLAR })
+    } else {
+      console.warn('Missing ETH Address for customerID: ', b.customerId, '. Total spend: ', b.totalSpent)
+    }
+  }
+
+
+  // Merge duplicate ETH address entries
+  const airdropList = _(recipients)
+    .groupBy('ethAddress')
+    .mapValues(
+      receipts => receipts.reduce((acc, r) => ({
+        tokenAmount: acc.tokenAmount + r.tokenAmount,
+        ethAddress: r.ethAddress,
+      }), { tokenAmount: 0, ethAddress: '' })
+    ).values().value();
+
+
+  const tokensToDistribute = airdropList.reduce((sum, { tokenAmount }) => sum += tokenAmount, 0)
+
+  const output = airdropList.filter(t => t.tokenAmount > 0).map(t => `${t.ethAddress},${numberToWei(t.tokenAmount)}`)
+  // Output result to console as CSV
+  console.log(output.join('\n'));
+  console.log({ totalSales, totalTokens: totalSales * BUYER_ROBOT_PER_DOLLAR, tokensToDistribute })
+};
+
+const getEthAddressForCustomer = async (customerId: number): Promise<string | null> => {
+  const QUERY = gql`
+    query getCustomer($customerId: ID!) {
+      customer(id: $customerId) {
+        metafield(key: "ethereum_address", namespace: "cf_app") {
+          value
+        }
+      }
+    }
+  `
+  const data = await client.request(QUERY, { customerId: `gid://shopify/Customer/${customerId}` });
+  const value = data.customer.metafield?.value;
+
+  let ethAddress = /0x[a-fA-F0-9]{40}/.exec(value)?.[0];
+  const ensName = /\w+\.eth/.exec(value)?.[0];
+
+  if (ensName) {
+    console.log(`Fetching ETH address for ENS`, ensName);
+    try {
+      ethAddress = await web3.eth.ens.getAddress(ensName);
+      console.log({ ethAddress, ensName });
+    } catch (e) {
+      console.warn("Unable to resolve ETH address for ENS: ", ensName);
+    }
+  }
+
+  if (!ethAddress) return null;
+
+  if (!web3.utils.isAddress(ethAddress)) {
+    console.log("Invalid ETH Address: ", { ethAddress, customerId });
+    return null;
+  }
+
+  return ethAddress;
+}
+
+
+const calculateDesignerAllocation = () => {
+  const products = _(ALL_ORDERS)
+    .groupBy('product_id')
+    .mapValues(
+      orders => orders.reduce((acc, order) => ({
+        totalSales: acc.totalSales + order.net_sales,
+        productId: order.product_id,
+        vendor: order.product_vendor,
+        title: order.product_title,
+      }), { totalSales: 0, productId: 0, vendor: '', title: '' })
+    ).values().value();
+
+
+  const tokensToDistribute = products.reduce((sum, { totalSales }) => sum += totalSales, 0) * DESIGNER_ROBOT_PER_DOLLAR
+
+
+  const output = products.map(t => `${t.productId},${t.title},${t.totalSales.toFixed(4)},${t.totalSales * DESIGNER_ROBOT_PER_DOLLAR}`)
+  // Output result to console as CSV
+  console.log(`product_id,title,totalSales,tokenAmount\n${output.join('\n')}`);
+  console.log({ tokensToDistribute});
+
+};
+
+
+calculateDesignerAllocation()
+
+calculateBuyerAllocation();
