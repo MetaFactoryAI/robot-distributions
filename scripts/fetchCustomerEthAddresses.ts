39d03035d88c30e9d54754f52cab0e487bdaf446
Hammad Jutt <jutt@ualberta.ca>
2021-08-26 20:20:41 -0600

    Finalize August Distro

--- scripts/fetchCustomerEthAddresses.ts
+++ scripts/fetchCustomerEthAddresses.ts
@@ -2,8 +2,10 @@ import _ from 'lodash';
 import { getEthAddressForCustomer } from '../lib/api';
 import fs from 'fs';
 import { ALL_ORDERS } from '../data';
+import CustomerAddresses from '../data/customerEthAddresses.json';
 
 const fetchCustomerEthAddresses = async () => {
+  const existingAddresses = _.keyBy(CustomerAddresses, 'customerId');
   const customerIds = _(ALL_ORDERS)
     .filter((c) => Boolean('customer_id' in c && c.customer_id))
     .groupBy('customer_id')
@@ -12,6 +14,12 @@ const fetchCustomerEthAddresses = async () => {
 
   const customers = [];
   for (const id of customerIds) {
+    if (existingAddresses[id]) {
+      console.log('customer has address: ', id);
+      customers.push(existingAddresses[id]);
+      continue;
+    }
+
     try {
       const ethAddress = await getEthAddressForCustomer(parseInt(id, 10));
       if (ethAddress) {
